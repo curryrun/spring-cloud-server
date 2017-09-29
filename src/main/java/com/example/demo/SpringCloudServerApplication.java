bd2c98af490ee7edbc505875178b950464affdf6
zhangdongrun <zhangdongrun@finupgroup.com>
2017-09-29 19:42:34 +0800

    修改为config-server

--- src/main/java/com/example/demo/SpringCloudServerApplication.java
+++ src/main/java/com/example/demo/SpringCloudServerApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringCloudServerApplication {
 
 	public static void main(String[] args) {
